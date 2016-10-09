e38b6b0bbc8ab31c1bcc764f787832b03a4f52b0
Fajar Yudha Pratama <fajaryudha23@gmail.com>
2016-10-09 15:09:47 +0700

    startActivity Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/xirpl3011/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl3011/intent1/MainActivity.java
@@ -1,13 +1,25 @@
 package id.sch.smktelkom_mlg.xirpl3011.intent1;
 
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, HasilActivity.class));
+            }
+        });
     }
 }
